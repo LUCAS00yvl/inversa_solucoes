0d83f2c9d9b7e4417c6bcf3ed8a83c9a166e3531
LUCAS00yvl <gcorreia173@gmail.com>
2025-05-22 22:48:30 -0300

    Primeiro commit

---
+++ inversa.js
@@ -0,0 +1,30 @@
+// Carrossel de destaques
+let index = 0;
+const slides = document.querySelectorAll(".carousel-slide");
+
+function showSlide(i) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[i].classList.add("active");
+}
+
+function nextSlide() {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+}
+
+setInterval(nextSlide, 3000);
+
+// Slideshow automático
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  const slides = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1; }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000);
+}
